83dd172a22cfd4fff8f8ede8d1f13b8981818c0c
David-E91 <david-e91@live.com>
2016-02-18 10:10:52 +0100

    Fenster
    - erstes Fenster

--- FiniteFielts/src/crypto/conductor/Conductor.java
+++ FiniteFielts/src/crypto/conductor/Conductor.java
@@ -1,16 +1,80 @@
 package crypto.conductor;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Toolkit;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 import crypto.polynom.Polynom;
 import crypto.time.measurement.TimeIt;
+//-----------------------------------------------------------------------------------------
+public class Conductor extends javax.swing.JFrame {
+	private static final long serialVersionUID = 3532667781001479319L;
 
-public class Conductor {
+	private Toolkit t;
+	private int x=0 ,y=0 ,width=800, height=600;
+	
+// Komponenten--------------------------------------------------------------
+	private JTextArea TextBox;
+	private JTextField Feld;
+	private JButton Knopf;
+	private JButton Knopf2;
+//--------------------------------------------------------------------------	
+	public Conductor(){
+		t = Toolkit.getDefaultToolkit();
+		Dimension d=t.getScreenSize();
+		x = (int) ((d.getWidth() - width) / 2);
+		y = (int) ((d.getHeight() - height) /2);
+		
+		
+		setTitle("Irreduzibles Polynom");
+		setBounds(x,y,width,height);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.initComponents();
+		setVisible(true);
+	}
+//------------------------------------------------------------------------------------------	
+	private void initComponents(){
+		this.getContentPane().setLayout(null);
+		
+// J Komponenten-----------------------------------------------------------------------------
+		this.TextBox = new JTextArea();
+		this.TextBox.setBounds(50,50,100,200);
+		
+		this.Feld = new JTextField();
+		this.Feld.setBounds(200,50,100,200);
+		
+		this.Knopf = new JButton("Knopf ");
+		this.Knopf.setBounds(300,50,100,50);
+		
+		this.Knopf2 = new JButton("Knopf ");
+		this.Knopf2.setBounds(300,100,100,50);
+//-------------------------------------------------------------------------------------------		
+
+//--------------Anzeige der Komponenten------------------------------------------------------                             
+		this.getContentPane().add(this.TextBox);
+		this.getContentPane().add(this.Feld);
+	    this.getContentPane().add(this.Knopf);
+	    this.getContentPane().add(this.Knopf2);
+	    
+	}
+//--------------------------------------------------------------------------------------------	
 
 	public static void main(String[] args) {
 		// Fi = p^n
-		int n = 3;
+		int n=1;
+		
+		//Swing fenster erstellen
+		new Conductor();
 		// get generating Polynoms
 		List<Integer> prim = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
 				73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181,
@@ -40,3 +104,4 @@ public class Conductor {
 
 	}
 }
+
